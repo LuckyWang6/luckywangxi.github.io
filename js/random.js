d64e47b040fe0d19f97802f3eb81ea7d2d0a7cb9
luckywangxi <2643589579@qq.com>
2022-12-04 16:34:25 +0800

    Site updated: 2022-12-04 16:34:25

--- js/random.js
+++ js/random.js
@@ -1,8 +1,11 @@
-hexo.extend.filter.register('after_render:html', function (data) {
-  const posts = []
-  hexo.locals.get('posts').map(function (post) {
-    if (post.random !== false) posts.push(post.path)
-  })
-  data += `<script>var posts=${JSON.stringify(posts)};function toRandomPost(){ window.pjax ? pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]) : window.open('/'+posts[Math.floor(Math.random()*posts.length)], "_self"); };</script>`
-  return data
+hexo.extend.generator.register('random', function (locals) {
+    const config = hexo.config.random || {}
+    const posts = []
+    for (const post of locals.posts.data) {
+        if (post.random !== false) posts.push(post.path)
+    }
+    return {
+        path: config.path || '/js/random.js',
+        data: `var posts=${JSON.stringify(posts)};function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};`
+    }
 })
